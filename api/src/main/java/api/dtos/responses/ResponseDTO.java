db3be29a4fee7ed1d9d622e528570f52b8576576
jvitor07 <jvitor2000@outlook.com>
2023-02-05 22:20:19 -0300

    feature(#1:backend): Adiciona anotação `@JsonProperty` para serializar os métodos na resposta da API.

--- api/src/main/java/api/dtos/responses/ResponseDTO.java
+++ api/src/main/java/api/dtos/responses/ResponseDTO.java
@@ -1,5 +1,6 @@
 package api.dtos.responses;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,10 +18,12 @@ public class ResponseDTO<T> {
     private List<String> messages;
     private List<String> errors;
 
+    @JsonProperty
     public Boolean hasMessages() {
         return Boolean.TRUE.equals(Objects.nonNull(this.messages) && !this.messages.isEmpty());
     }
 
+    @JsonProperty
     public Boolean hasErrors() {
         return Boolean.TRUE.equals(Objects.nonNull(this.errors) && !this.errors.isEmpty());
     }
