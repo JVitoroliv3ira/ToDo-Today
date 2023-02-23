a064c7a1287e8cb18acca35f342e90ffe297d58f
jvitor07 <jvitoroliveira0711@gmail.com>
2023-02-23 18:50:30 -0300

    feature(#3:backend): Cria Repository para o modelo TaskList

---
+++ api/src/main/java/api/repositories/TaskListRepository.java
@@ -0,0 +1,9 @@
+package api.repositories;
+
+import api.models.TaskList;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskListRepository extends JpaRepository<TaskList, Long> {
+}
