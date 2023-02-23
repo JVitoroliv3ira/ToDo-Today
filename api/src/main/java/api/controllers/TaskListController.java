3aaab0f60fda19c9d46f5f7286e0daf900c08073
jvitor07 <jvitoroliveira0711@gmail.com>
2023-02-23 19:01:57 -0300

    feature(#3:backend): Cria controller para TaskList

---
+++ api/src/main/java/api/controllers/TaskListController.java
@@ -0,0 +1,10 @@
+package api.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping(path = "/api/v1/task-list")
+@RestController
+public class TaskListController {
+    
+}
