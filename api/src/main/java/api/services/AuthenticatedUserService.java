e08ade24ca84c1977a6067d8e8dba0f90904a2d7
jvitor07 <jvitoroliveira0711@gmail.com>
2023-02-23 19:57:30 -0300

    feature(#3:backend): Cria um service para o usuário autenticado

---
+++ api/src/main/java/api/services/AuthenticatedUserService.java
@@ -0,0 +1,17 @@
+package api.services;
+
+import api.dtos.DetailsDTO;
+import api.models.User;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticatedUserService {
+
+    public static User getAuthenticatedUser() {
+        return getAuthenticatedUserDetails().getUser();
+    }
+    public static DetailsDTO getAuthenticatedUserDetails() {
+        return (DetailsDTO) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    }
+}
