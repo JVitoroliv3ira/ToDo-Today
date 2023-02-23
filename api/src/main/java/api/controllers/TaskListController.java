ed480d5f3782e163f5915eb181525802337b970d
jvitor07 <jvitoroliveira0711@gmail.com>
2023-02-23 20:18:12 -0300

    feature(#3:backend): Implementa endpoint para cadastrar a entidade TaskList

--- api/src/main/java/api/controllers/TaskListController.java
+++ api/src/main/java/api/controllers/TaskListController.java
@@ -1,10 +1,32 @@
 package api.controllers;
 
+import api.dtos.requests.tasklist.TaskListCreationRequestDTO;
+import api.dtos.responses.ResponseDTO;
+import api.dtos.responses.TaskListResponseDTO;
+import api.models.TaskList;
+import api.services.AuthenticatedUserService;
+import api.services.TaskListService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.springframework.http.HttpStatus.CREATED;
+
+@RequiredArgsConstructor
 @RequestMapping(path = "/api/v1/task-list")
 @RestController
 public class TaskListController {
-    
+    private final TaskListService taskListService;
+
+    @PostMapping(path = "/create")
+    public ResponseEntity<ResponseDTO<TaskListResponseDTO>> create(@Valid @RequestBody TaskListCreationRequestDTO request) {
+        TaskList result = this.taskListService.create(request.convert(), AuthenticatedUserService.getAuthenticatedUser());
+        return ResponseEntity
+                .status(CREATED)
+                .body(new ResponseDTO<>(new TaskListResponseDTO(result), null, null));
+    }
 }
