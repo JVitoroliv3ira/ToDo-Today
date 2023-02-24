33f8d7a33e16cf6bab766365b2d15ffc81fa997c
jvitor07 <jvitoroliveira0711@gmail.com>
2023-02-23 21:06:54 -0300

    feature(#3:backend): Cria método para buscar listas de tarefas de forma paginada

--- api/src/main/java/api/services/TaskListService.java
+++ api/src/main/java/api/services/TaskListService.java
@@ -6,6 +6,9 @@ import api.models.User;
 import api.repositories.TaskListRepository;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -17,6 +20,11 @@ public class TaskListService implements ICrudService<TaskList, Long, TaskListRep
     public TaskList create(TaskList taskList, User creator) {
         this.associateCreatorWithTaskList(creator, taskList);
         return this.create(taskList);
+    }
+
+    public Page<TaskList> paginate(Long creatorId, Integer page, Integer pageSize) {
+        Pageable pageable = PageRequest.of(page, pageSize);
+        return this.repository.findAll(creatorId, pageable);
     }
 
     @Override
