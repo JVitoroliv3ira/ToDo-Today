2fc334aa6bee25c29208cce4f9521598326daf72
jvitor07 <jvitoroliveira0711@gmail.com>
2023-02-23 21:06:32 -0300

    feature(#3:backend): Cria método para busca das listas de tarefas de forma paginada

--- api/src/main/java/api/repositories/TaskListRepository.java
+++ api/src/main/java/api/repositories/TaskListRepository.java
@@ -1,9 +1,14 @@
 package api.repositories;
 
 import api.models.TaskList;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TaskListRepository extends JpaRepository<TaskList, Long> {
+    @Query("SELECT t FROM TaskList t WHERE t.creator.id = ?1")
+    Page<TaskList> findAll(Long creatorId, Pageable pageable);
 }
